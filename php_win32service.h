bca894447d64273f0a7c3d8f98ba058eb97a0361
Credomane <github@credomane.com>
2016-05-20 11:05:42 -0500

    Update to how dwControlsAccepted are determined
    Uncommented some constants as well.

--- php_win32service.h
+++ php_win32service.h
@@ -2,7 +2,7 @@
   +----------------------------------------------------------------------+
   | PHP Version 5                                                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2011 The PHP Group                                |
+  | Copyright (c) 1997-2015 The PHP Group                                |
   +----------------------------------------------------------------------+
   | This source file is subject to version 3.0 of the PHP license,       |
   | that is bundled with this package in the file LICENSE, and is        |
@@ -13,6 +13,7 @@
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
   | Author: Wez Furlong <wez@php.net>                                    |
+  | Author: Credomane                                                    |
   +----------------------------------------------------------------------+
 */
 
@@ -24,7 +25,7 @@
 extern zend_module_entry win32service_module_entry;
 #define phpext_win32service_ptr    &win32service_module_entry
 
-#define PHP_WIN32SERVICE_VERSION "0.1.0-dev"
+#define PHP_WIN32SERVICE_VERSION "0.1.3-dev"
 
 #ifndef PHP_WIN32
 # error This extension is for win32 only
--- php_win32service_int.h
+++ php_win32service_int.h
@@ -2,7 +2,7 @@
   +----------------------------------------------------------------------+
   | PHP Version 5                                                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2011 The PHP Group                                |
+  | Copyright (c) 1997-2015 The PHP Group                                |
   +----------------------------------------------------------------------+
   | This source file is subject to version 3.0 of the PHP license,       |
   | that is bundled with this package in the file LICENSE, and is        |
@@ -13,6 +13,7 @@
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
   | Author: Wez Furlong <wez@php.net>                                    |
+  | Author: Credomane                                                    |
   +----------------------------------------------------------------------+
 */
 
